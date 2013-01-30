4b7414e056dd4a4ffe829b0ed3d3f808ef5dabf4
machipeddy9@gmail.com <machipeddy9@gmail.com>
2013-01-30 20:28:09 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Port::Application.config.secret_token = '23a98450397ee92903b235d474dab766be2b07d33207f3ee30081ba001d8b0de16a912215e5cb9939f2c7d23807bb1b03caf5145d2ba721aded1f6fc8487684e'
